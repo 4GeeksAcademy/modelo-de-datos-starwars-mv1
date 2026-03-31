f980fb4cc8d094ce3a72535c2a2f24aa005e1ac1
miguelvas <44248779+miguelvas@users.noreply.github.com>
2026-03-31 02:03:25 +0000

    Primera Entrega

--- src/models.py
+++ src/models.py
@@ -1,19 +1,67 @@
+from eralchemy2 import render_er
+import os
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = 'user'
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    username: Mapped[str] = mapped_column(
+        String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
+    password: Mapped[str] = mapped_column(String(80), nullable=False)
+
+    # Un usuario tiene muchos favoritos
+    favorites: Mapped[List["Favorite"]] = relationship(back_populates="user")
 
 
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+class People(db.Model):
+    __tablename__ = 'people'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    height: Mapped[str] = mapped_column(String(20))
+    mass: Mapped[str] = mapped_column(String(20))
+    hair_color: Mapped[str] = mapped_column(String(50))
+
+
+class Planet(db.Model):
+    __tablename__ = 'planet'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False)
+    climate: Mapped[str] = mapped_column(String(50))
+    terrain: Mapped[str] = mapped_column(String(50))
+    population: Mapped[str] = mapped_column(String(50))
+
+
+class Favorite(db.Model):
+    __tablename__ = 'favorite'
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    # Relación con el Usuario
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"), nullable=False)
+    user: Mapped["User"] = relationship(back_populates="favorites")
+
+    # Relación con Personajes
+    people_id: Mapped[int] = mapped_column(
+        ForeignKey("people.id"), nullable=True)
+    people: Mapped["People"] = relationship()
+
+    # Relación con Planetas
+    planet_id: Mapped[int] = mapped_column(
+        ForeignKey("planet.id"), nullable=True)
+    planet: Mapped["Planet"] = relationship()
+
+
+if __name__ == "__main__":
+    try:
+        render_er(db.Model, 'diagram.png')
+        print("¡Éxito! El diagrama de Star Wars se ha generado en diagram.png")
+    except Exception as e:
+        print("Hubo un error al generar el diagrama:")
+        print(e)
